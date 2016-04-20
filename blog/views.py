7148d765dbcfa01cb5f2f9f58c0956ac4483e7d8
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-04-20 12:55:09 +0200

    Reorganized code

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -7,7 +7,7 @@ def post_list(request):
     posts = Post.objects.filter(
         published_date__lte = timezone.now()
     ).order_by(
-        'published_date'
+        '-published_date'
     )
     return render(request, 'blog/post_list.html', {'posts': posts})
 
@@ -16,5 +16,14 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
